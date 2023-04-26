a357608af77e3b7dd619eb42447b3ba9804c497c
dmtrack <dmtrack.dev@gmail.com>
2023-04-26 21:11:41 +0500

    refactor: db params

--- dist/db/config.js
+++ dist/db/config.js
@@ -1,28 +1,26 @@
-'use strict';
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
-const stringfield_1 = require('./models/field_types/stringfield');
-const sequelize_typescript_1 = require('sequelize-typescript');
-const user_1 = require('./models/user');
-const dotenv_1 = __importDefault(require('dotenv'));
-const collection_1 = require('./models/collection');
-const tag_item_1 = require('./models/tag_item');
-const item_1 = require('./models/item');
-const comment_1 = require('./models/comment');
-const field_1 = require('./models/field');
-const fieldtype_1 = require('./models/fieldtype');
-const user_access_1 = require('./models/user_access');
-const token_1 = require('./models/token');
-const textfield_1 = require('./models/field_types/textfield');
-const booleanfield_1 = require('./models/field_types/booleanfield');
-const datefield_1 = require('./models/field_types/datefield');
-const numfield_1 = require('./models/field_types/numfield');
-const theme_1 = require('./models/theme');
-const like_1 = require('./models/like');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const stringfield_1 = require("./models/field_types/stringfield");
+const sequelize_typescript_1 = require("sequelize-typescript");
+const user_1 = require("./models/user");
+const dotenv_1 = __importDefault(require("dotenv"));
+const collection_1 = require("./models/collection");
+const tag_item_1 = require("./models/tag_item");
+const item_1 = require("./models/item");
+const comment_1 = require("./models/comment");
+const field_1 = require("./models/field");
+const fieldtype_1 = require("./models/fieldtype");
+const user_access_1 = require("./models/user_access");
+const token_1 = require("./models/token");
+const textfield_1 = require("./models/field_types/textfield");
+const booleanfield_1 = require("./models/field_types/booleanfield");
+const datefield_1 = require("./models/field_types/datefield");
+const numfield_1 = require("./models/field_types/numfield");
+const theme_1 = require("./models/theme");
+const like_1 = require("./models/like");
 dotenv_1.default.config();
 const connection = new sequelize_typescript_1.Sequelize({
     dialect: 'postgres',
