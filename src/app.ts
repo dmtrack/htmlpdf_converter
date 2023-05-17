df2c34b5452e83ad16bd3f23f6b8b255c54c8405
dmtrack <dmtrack.dev@gmail.com>
2023-05-17 07:04:52 +0500

    refactor: db settings

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use('/dev', devRouter);
 Socket.onEvents();
 
 connection
-    .sync({ alter: true })
+    .sync({ force: true })
     .then(async () => {
         deleteAllIndexes('collections');
         deleteAllIndexes('items');
