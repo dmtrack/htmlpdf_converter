61fe49c9bc9b31ddd4ddc858287e1a9b47aa13fb
dmtrack <dmtrack.dev@gmail.com>
2023-08-19 19:59:53 +0500

    ref/test

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,5 @@ let { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-    // Changes the cache location for Puppeteer.
     cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
